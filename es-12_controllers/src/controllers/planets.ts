b054dac75ba6772f21cc47233da24900ae666249
Alfio Puglisi <puglisi_algio@ciao.it>
2023-06-26 16:10:58 +0200

    JWT parte 1

--- es-12_controllers/src/controllers/planets.ts
+++ es-12_controllers/src/controllers/planets.ts
@@ -1,25 +1,7 @@
 import { Request, Response } from "express";
 import Joi from "joi";
-import pgPromise from "pg-promise";
+import { db } from "../db";
 
-const db = pgPromise()("postgres://postgres:postgres@localhost:5432/video");
-
-const setupDB = async () => {
-  await db.none(`
-        DROP TABLE IF EXISTS planets
-
-        CREATE TABLE planets (
-          id SERIAL NOT NULL PRIMARY KEY,
-          name TEXT NOT NULL,
-          image TEXT
-        )
-  `);
-
-  await db.none(`INSERT INTO planets planet VALUES ('Terra')`);
-  await db.none(`INSERT INTO planets planet VALUES ('Marte')`);
-};
-
-setupDB();
 
 type Planet = {
   id: number;
